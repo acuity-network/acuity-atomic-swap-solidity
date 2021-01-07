aa36cb51e49f96d8bbb1528de8363aa366e1c244
Jonathan Brown <jbrown@acuity.social>
2021-01-08 01:46:43 +0700

    Add scripts.

---
+++ scripts/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "*** Initializing Dapp Tools"
+
+curl -L https://nixos.org/nix/install | sh
+. ~/.nix-profile/etc/profile.d/nix.sh
+curl https://dapp.tools/install | sh
+nix-env -f https://github.com/dapphub/dapptools/archive/master.tar.gz -iA solc-static-versions.solc_0_7_5
---
+++ scripts/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+dapp --use solc:0.7.5 test
