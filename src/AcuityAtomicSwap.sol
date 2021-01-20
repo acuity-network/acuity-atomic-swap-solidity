22bb206f7f50ed3ff9dcfa0b18216cfcf152db1e
Jonathan Brown <jbrown@acuity.social>
2021-01-20 19:00:28 +0700

    Emit events.

--- src/AcuityAtomicSwap.sol
+++ src/AcuityAtomicSwap.sol
@@ -21,6 +21,41 @@ contract AcuityAtomicSwap {
 
     mapping (bytes32 => SellLock) hashedSecretSellLock;
 
+    /**
+     * @dev
+     */
+    event CreateOrder(address indexed seller, bytes32 indexed orderId, uint price, uint value);
+
+    /**
+     * @dev
+     */
+    event LockBuy(bytes32 indexed orderId, bytes32 indexed hashedSecret, address indexed seller, uint value, uint timeout);
+
+    /**
+     * @dev
+     */
+    event LockSell(bytes32 indexed orderId, bytes32 indexed hashedSecret, address indexed seller, uint value, uint timeout);
+
+    /**
+     * @dev
+     */
+    event UnlockSell(bytes32 indexed hashedSecret, address indexed dest, uint value, bytes32 secret);
+
+    /**
+     * @dev
+     */
+    event TimeoutSell(bytes32 indexed hashedSecret, bytes32 indexed orderId, uint value);
+
+    /**
+     * @dev
+     */
+    event UnlockBuy(bytes32 indexed hashedSecret, address indexed seller, uint value);
+
+    /**
+     * @dev
+     */
+    event TimeoutBuy(bytes32 indexed hashedSecret, address indexed dest, uint value);
+
     /*
      * Called by seller.
      */
@@ -29,7 +64,8 @@ contract AcuityAtomicSwap {
         orderId = keccak256(abi.encodePacked(msg.sender, price));
         // Get order.
         orderIdAmount[orderId] += msg.value;
-        // log info
+        // Log info.
+        emit CreateOrder(msg.sender, orderId, price, msg.value);
     }
 
     /*
@@ -40,7 +76,8 @@ contract AcuityAtomicSwap {
         lock.seller = seller;
         lock.amount = msg.value;
         lock.timeout = timeout;
-        // log info
+        // Log info.
+        emit LockBuy(orderId, hashedSecret, seller, msg.value, timeout);
     }
 
     /*
@@ -56,6 +93,8 @@ contract AcuityAtomicSwap {
         hashedSecretSellLock[hashedSecret].amount = amount;
         hashedSecretSellLock[hashedSecret].timeout = timeout;
         hashedSecretSellLock[hashedSecret].orderId = orderId;
+        // Log info.
+        emit LockSell(orderId, hashedSecret, msg.sender, amount, timeout);
     }
 
     /*
@@ -70,6 +109,8 @@ contract AcuityAtomicSwap {
         assembly {
             pop(call(not(0), _sender, value, 0, 0, 0, 0))
         }
+        // Log info.
+        emit UnlockSell(hashedSecret, msg.sender, value, secret);
     }
 
     /*
@@ -80,8 +121,11 @@ contract AcuityAtomicSwap {
         bytes32 orderId = keccak256(abi.encodePacked(msg.sender, price));
         require (hashedSecretSellLock[hashedSecret].orderId == orderId, "Wrong orderId.");
         require (hashedSecretSellLock[hashedSecret].timeout >= block.timestamp, "Lock not timed out.");
-        orderIdAmount[orderId] += hashedSecretSellLock[hashedSecret].amount;
+        uint amount = hashedSecretSellLock[hashedSecret].amount;
+        orderIdAmount[orderId] += amount;
         delete hashedSecretSellLock[hashedSecret];
+        // Log info.
+        emit TimeoutSell(hashedSecret, orderId, amount);
     }
 
     /*
@@ -96,6 +140,8 @@ contract AcuityAtomicSwap {
         assembly {
             pop(call(not(0), seller, value, 0, 0, 0, 0))
         }
+        // Log info.
+        emit UnlockBuy(hashedSecret, seller, value);
     }
 
     /*
@@ -110,6 +156,8 @@ contract AcuityAtomicSwap {
         assembly {
             pop(call(not(0), dest, value, 0, 0, 0, 0))
         }
+        // Log info.
+        emit TimeoutBuy(hashedSecret, dest, value);
     }
 
 }
