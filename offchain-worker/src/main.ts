74c15fd3d349071cb7a885f3ec10c3747f11fa17
Jonathan Brown <jbrown@acuity.social>
2021-03-03 17:30:11 +0700

    Implement total issuance.

--- offchain-worker/src/main.ts
+++ offchain-worker/src/main.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 let app = express()
+import { ApiPromise, WsProvider } from '@polkadot/api'
+import BN from 'bn.js'
+
+let api: any
 
 
 async function startServer() {
   app.get('/', async function (req: any, res: any) {
     res.json({
-      blockNumber: 1234,
+      totalIssuance: (await api.query.balances?.totalIssuance()).div(new BN('1000000000000000000')).toString(),
     })
   })
 
@@ -15,6 +19,22 @@ async function startServer() {
 }
 
 async function start() {
+
+  let wsProvider = new WsProvider('wss://acuity.social:9961')
+  ApiPromise.create({ provider: wsProvider }).then(async newApi => {
+    api = newApi
+    await api.isReady
+
+    // Retrieve the chain & node information information via rpc calls
+      const [chain, nodeName, nodeVersion] = await Promise.all([
+        api.rpc.system.chain(),
+        api.rpc.system.name(),
+        api.rpc.system.version()
+      ]);
+
+      console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+  })
+
   startServer()
 }
 
