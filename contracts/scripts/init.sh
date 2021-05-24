83c94efef874875c7b9a3015b1e58bcdd7620a38
Jonathan Brown <jbrown@acuity.social>
2021-05-24 16:02:10 +0700

    Use Solidity 0.8.3

--- contracts/scripts/init.sh
+++ contracts/scripts/init.sh
@@ -7,3 +7,4 @@ echo "*** Initializing Dapp Tools"
 curl -L https://nixos.org/nix/install | sh
 . ~/.nix-profile/etc/profile.d/nix.sh
 curl https://dapp.tools/install | sh
+nix-env -f https://github.com/dapphub/dapptools/archive/master.tar.gz -iA solc-static-versions.solc_0_8_3
--- contracts/scripts/test.sh
+++ contracts/scripts/test.sh
@@ -4,4 +4,4 @@ set -e
 
 . ~/.nix-profile/etc/profile.d/nix.sh
 export DAPP_SOLC=/usr/bin/solc
-dapp test --verbose
+dapp --use solc:0.8.3 test --verbose
