eb3795f0d52454fd9340282e10096ce9540054d7
Jonathan Brown <jbrown@acuity.social>
2021-05-28 14:53:45 +0700

    Optimize for 10,000 runs.

--- contracts/scripts/test.sh
+++ contracts/scripts/test.sh
@@ -3,5 +3,5 @@
 set -e
 
 . ~/.nix-profile/etc/profile.d/nix.sh
-export DAPP_SOLC=/usr/bin/solc
-dapp --use solc:0.8.3 --verbose --optimize test 
+export DAPP_BUILD_OPTIMIZE_RUNS=10000
+dapp --use solc:0.8.3 --verbose --optimize test
