119cfbb65bac9b0bf1a25630c3cc876024c0dc87
Jonathan Brown <jbrown@acuity.social>
2021-05-30 09:27:04 +0700

    Add build script.

---
+++ contracts/scripts/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+. ~/.nix-profile/etc/profile.d/nix.sh
+export DAPP_BUILD_OPTIMIZE_RUNS=10000
+dapp --use solc:0.8.3 --verbose --optimize build
+solang -o out ../src/AcuityAtomicSwapSell.sol src/AcuityAtomicSwapBuy.sol
