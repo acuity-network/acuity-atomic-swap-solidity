15c09c3148f848ed064ac01569a1993c186bf079
Jonathan Brown <jbrown@acuity.social>
2022-06-23 13:32:55 +0700

    Update deploy scripts.

---
+++ scripts/deploy-avalanche-fuji.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+./scripts/build.sh
+mkdir -p ~/.ethereum/keystore/
+cp ./scripts/UTC--2021-09-03T23-02-54.725253244Z--a686954df1a23c379a9ce4b37b18c60f9a8e8c2f ~/.ethereum/keystore/
+export ETH_FROM=0xa686954df1a23c379a9ce4b37b18c60f9a8e8c2f
+export ETH_PASSWORD=./scripts/password
+export ETH_GAS=8000000
+export ETH_RPC_URL=https://api.avax-test.network/ext/bc/C/rpc
+. ~/.nix-profile/etc/profile.d/nix.sh
+dapp create AcuityAccount
+dapp create AcuityAtomicSwap
--- scripts/deploy-smartbch-amber.sh
+++ scripts/deploy-smartbch-amber.sh
@@ -7,8 +7,8 @@ mkdir -p ~/.ethereum/keystore/
 cp ./scripts/UTC--2021-09-03T23-02-54.725253244Z--a686954df1a23c379a9ce4b37b18c60f9a8e8c2f ~/.ethereum/keystore/
 export ETH_FROM=0xa686954df1a23c379a9ce4b37b18c60f9a8e8c2f
 export ETH_PASSWORD=./scripts/password
-export ETH_GAS=40000000
-export ETH_RPC_URL=https://rinkeby.arbitrum.io/rpc
+export ETH_GAS=8000000
+export ETH_RPC_URL=http://35.220.203.194:8545
 . ~/.nix-profile/etc/profile.d/nix.sh
 dapp create AcuityAccount
 dapp create AcuityAtomicSwap
