18227af8bda0f900455ed1897abeb3d5efd00d32
Jonathan Brown <jbrown@acuity.social>
2022-08-28 22:42:27 +0700

    Add ERC20 events.

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -15,4 +15,6 @@ interface ERC20 {
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
     function approve(address _spender, uint256 _value) external returns (bool);
     function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
