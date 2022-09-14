88b7ca180e2d769d37178a4aaa16c69a1fb228a7
Jonathan Brown <jbrown@acuity.social>
2022-09-15 04:53:40 +0700

    Implement account balance multiple getters.

--- src/AcuityRPC.sol
+++ src/AcuityRPC.sol
@@ -5,25 +5,42 @@ import "./ERC20.sol";
 
 contract AcuityRPC {
 
+    function getAccountBalances(address[] calldata accounts) view external returns (uint[] memory values) {
+        values = new uint[](accounts.length);
+
+        for (uint i = 0; i != accounts.length; i++) {
+            values[i] = accounts[i].balance;
+        }
+    }
+
     function getStaticTokenMetadata(ERC20 token) view external returns (string memory name, string memory symbol, uint8 decimals) {
         name = token.name();
         symbol = token.symbol();
         decimals = token.decimals();
     }
 
-    function getTokenBalances(address owner, ERC20[] calldata tokens) view external returns (uint[] memory values) {
-        values = new uint[](tokens.length);
+    function getTokenAccountBalances(ERC20 token, address[] calldata accounts) view external returns (uint[] memory values) {
+        values = new uint[](accounts.length);
 
-        for (uint i = 0; i < tokens.length; i++) {
-            values[i] = tokens[i].balanceOf(owner);
+        for (uint i = 0; i != accounts.length; i++) {
+            values[i] = token.balanceOf(accounts[i]);
         }
     }
 
-    function getTokenAllowances(address owner, address spender, ERC20[] calldata tokens) view external returns (uint[] memory values) {
+    function getAccountTokenBalances(address account, ERC20[] calldata tokens) view external returns (uint[] memory values) {
         values = new uint[](tokens.length);
 
-        for (uint i = 0; i < tokens.length; i++) {
-            values[i] = tokens[i].allowance(owner, spender);
+        for (uint i = 0; i != tokens.length; i++) {
+            values[i] = tokens[i].balanceOf(account);
         }
     }
+
+    function getAccountTokenAllowances(address account, address spender, ERC20[] calldata tokens) view external returns (uint[] memory values) {
+        values = new uint[](tokens.length);
+
+        for (uint i = 0; i != tokens.length; i++) {
+            values[i] = tokens[i].allowance(account, spender);
+        }
+    }
+
 }
